3ff3dfdb8776334d9c924e3811b4395f728277cf
FranRom <maildefranz@gmail.com>
2017-11-17 10:32:46 +0100

    Iteration 3

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -100,4 +100,21 @@ router.post('/login', (req, res, next) => {
   });
 });
 
+//logout
+router.get('/logout', (req, res, next) => {
+  if (!req.session.currentUser) {
+    res.redirect('/');
+    return;
+  }
+
+  req.session.destroy((err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.redirect('/');
+  });
+});
+
 module.exports = router;
